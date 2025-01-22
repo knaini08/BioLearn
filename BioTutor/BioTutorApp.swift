0a78521e7877a7ab3866a1b2edc318d6cf243fd0
Kaveh Naini <kavehnaini1@gmail.com>
2025-01-21 23:03:18 -0800

    Initial Commit

---
+++ BioTutor/BioTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioTutorApp.swift
+//  BioTutor
+//
+//  Created by Kaveh Naini on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct BioTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
